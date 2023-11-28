67c6d697302416c3eb8ba794f8dee2ef67dec8bb
Samuel wanza <s.wanza@alustudent.com>
2023-11-28 06:29:22 +0200

    added model file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify
 import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+import numpy as np
 
 app = Flask(__name__)
 
@@ -7,6 +11,29 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# Define a route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get the image file from the request
+        image_file = request.files['image']
+
+        # Load and preprocess the image
+        img = image.load_img(image_file, target_size=(299, 299))  # Assuming InceptionV3 input size
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = preprocess_input(img_array)
+
+        # Perform prediction using the loaded model
+        predictions = loaded_model.predict(img_array)
+
+
+
+        # Return the predictions as JSON
+        return jsonify({'predictions': predictions.tolist()})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 if __name__ == '__main__':
