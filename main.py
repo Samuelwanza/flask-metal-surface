db7b51de44dd1ada59a91a1c132c20b63d9d9dd3
Samuel wanza <s.wanza@alustudent.com>
2023-11-28 18:42:58 +0200

    added more dependencies

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import os
 import requests
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 import numpy as np
+import tempfile
 
 app = Flask(__name__)
+CORS(app)
 
 # Download the model when the application starts
 model_url = 'https://drive.google.com/uc?id=1ay2Zkhi6e8Y5Z1W8AUP1z-t9UVvAK0P8'
 model_path = 'model.h5'  # Choose a local path to save the model file
-
+class_labels=['Crazing', 'Inclusion', 'Patches', 'Pitted', 'Rolled', 'Scratches']
 response = requests.get(model_url)
 with open(model_path, 'wb') as f:
     f.write(response.content)
@@ -26,12 +29,16 @@ def index():
 # Define a route for prediction
 @app.route('/predict', methods=['POST'])
 def predict():
+    print("Predict method called!")
+
     try:
         # Get the image file from the request
         image_file = request.files['image']
+        temp_file_path = tempfile.NamedTemporaryFile(delete=False).name
+        image_file.save(temp_file_path)
 
-        # Load and preprocess the image
-        img = image.load_img(image_file, target_size=(224, 244))  # Assuming InceptionV3 input size
+        # Load and preprocess the images
+        img = image.load_img(temp_file_path, target_size=(224, 224))  # Assuming the original input size of InceptionV3
         img_array = image.img_to_array(img)
         img_array = np.expand_dims(img_array, axis=0)
         img_array = preprocess_input(img_array)
@@ -39,11 +46,18 @@ def predict():
         # Perform prediction using the loaded model
         predictions = loaded_model.predict(img_array)
 
-        # Return the predictions as JSON
-        return jsonify({'predictions': predictions.tolist()})
+        # Get the index of the maximum value (predicted class)
+        predicted_class_index = np.argmax(predictions)
+
+        # Get the corresponding class label
+        predicted_class = class_labels[predicted_class_index]
+
+        print("Predicted class index:", predicted_class_index)
+        # Return the predicted class as JSON
+        return jsonify({'predicted_class': predicted_class})
 
     except Exception as e:
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(port=os.getenv("PORT", default=5000))
